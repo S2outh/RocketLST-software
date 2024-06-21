7730fa754d50abc44d314c3b1b730c0de6b4e5ec
Robin <robin.finkelmann@stud-mail.uni-wuerzburg.de>
2024-06-21 15:12:28 +0200

    Beta nix dev environment

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    }
+      @inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell {
+            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+	    buildInputs = [
+            pkgs.sdcc
+            pkgs.cc-tool
+            #pkgs.stdenv.cc.cc.lib
+          ];
+          shellHook = ''
+	        echo "Welcome in the OpenLST Shell!"
+          '';
+        };
+    };
+}
